34208c2befaf1d860ac029387079c8136ccad7d6
williamgyamfik <chester3gh@gmail.com>
2024-03-27 23:04:10 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import CountryTable from "../components/CountryTable"
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_DATA = gql`
+  query {
+    countries {
+      code,
+      name
+    }
+  }
+`
+
+export interface countryData {
+  name:string,
+  code: string
+  id:number 
+}
+
+
+
+const App: React.FC =()=> {
+  const { loading, error, data } = useQuery(GET_DATA);
+
+const countries :countryData[] = data?.countries || [] 
+
+if(loading){
+  return <p>Loading....</p>
+}
+
+if(error){
+  return <p>Error: {error.message}</p>
+}
+
+
+
+
+
+  return (
+    <>
+      <h1 className='text-black font-semibold'>Name : William Gyamfi Kumi .....YOLO Test</h1>
+      <CountryTable countries={countries}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import './App.css'
+import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache(),
+});
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+       <App />
+   </ApolloProvider>
+  </React.StrictMode>,
+)
