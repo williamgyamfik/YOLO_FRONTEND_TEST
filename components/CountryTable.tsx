ef2ec3cee4b5afe9320092432852512e16c7612d
williamgyamfik <chester3gh@gmail.com>
2024-03-28 00:31:15 +0200

    code review, Testing next

--- components/CountryTable.tsx
+++ components/CountryTable.tsx
@@ -1,97 +1,86 @@
 "use client";
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import { countryData } from '../src/App';
+import React from "react";
+import { useState } from "react";
+import { CountryData } from "../src/App";
 
+interface CountryTableProps {
+  countries: CountryData[];
+}
 
-interface countryDataProps {
-  countries : countryData[]
- }
+const CountryTable: React.FC<CountryTableProps> = ({ countries }) => {
+  const [searchValue, setSearchvalue] = useState<string>("");
+  const [filterValue, setFilteredValue] = useState<CountryData[]>(countries);
 
-
-const CountryTable:React.FC<countryDataProps> = ({countries}) => {
-
-const [searchValue,setSearchvalue] = useState<string>("")
-const [movieData,setMovieData] = useState<countryData[]>([])
-const [filterValue, setFilteredValue] = useState<countryData[]>([])
-
-
-useEffect(()=>{
-    setMovieData(countries)
-    setFilteredValue(countries)
-},[countries])
-
-
-
-const searchInputHandler =(e: React.ChangeEvent<HTMLInputElement>)=> {
-    
-   
-    const inputValue = e.target.value.toLowerCase().trim()
+  const searchInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value.toLowerCase().trim();
     const isNumber = !isNaN(parseFloat(inputValue));
 
-    if(isNumber){
-        alert("Input value must be string")
-        return
-    }else {
-        setSearchvalue(inputValue)
+    if (isNumber) {
+      alert("Input value must be string");
+      return;
+    } else {
+      setSearchvalue(inputValue);
     }
-    const filteredText = movieData.filter((value)=> {
-      return  value.code.toLowerCase().includes(inputValue) 
-    })
-     setFilteredValue(filteredText)
-
-}
-
-
+    const filteredText = countries.filter((value) => {
+      return value.code.toLowerCase().includes(inputValue);
+    });
+    setFilteredValue(filteredText);
+  };
 
   return (
-  <> 
-<form className="flex items-center max-w-sm mx-auto mb-5" >   
-    <label htmlFor="simple-search" className="sr-only">Search</label>
-    <div className="relative w-full">
-   <input value={searchValue} onChange={searchInputHandler} maxLength={2} type="text" id="simple-search" className="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by country code..." required />
-    </div>
-    
-</form>
+    <>
+      <form className="flex items-center max-w-sm mx-auto mb-5">
+        <label htmlFor="simple-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative w-full">
+          <input
+            value={searchValue}
+            onChange={searchInputHandler}
+            maxLength={2}
+            type="text"
+            id="simple-search"
+            className="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search by country code..."
+            required
+          />
+        </div>
+      </form>
 
-<div className="relative overflow-x-auto">
-  
-
-
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
             <tr>
-                <th scope="col" className="px-6 py-3">
-                    Country
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Code
-                </th>
-                
-            </tr>
-        </thead>
-        <tbody>
-           {filterValue.map((country,index)=>{
-            return (
-              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-               {country.name}
+              <th scope="col" className="px-6 py-3">
+                Country
               </th>
-              <td className="px-6 py-4">
-                 {country.code}
-              </td>
-              
-          </tr>
-            )
-           })}
-            
-        </tbody>
-    </table>
-</div>
+              <th scope="col" className="px-6 py-3">
+                Code
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterValue.map((country, index) => {
+              return (
+                <tr
+                  key={index}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {country.name}
+                  </th>
+                  <td className="px-6 py-4">{country.code}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-
-</>
-  )
-}
-
-export default CountryTable
+export default CountryTable;
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,43 @@
 import React from "react";
-import CountryTable from "../components/CountryTable"
-import { useQuery, gql } from '@apollo/client';
-
+import CountryTable from "../components/CountryTable";
+import { useQuery, gql } from "@apollo/client";
 
 const GET_DATA = gql`
   query {
     countries {
-      code,
+      code
       name
     }
   }
-`
+`;
 
-export interface countryData {
-  name:string,
-  code: string
-  id:number 
+export interface CountryData {
+  name: string;
+  code: string;
+  id: number;
 }
 
-
-
-const App: React.FC =()=> {
+const App: React.FC = () => {
   const { loading, error, data } = useQuery(GET_DATA);
 
-const countries :countryData[] = data?.countries || [] 
+  const countries: CountryData[] = data?.countries || [];
 
-if(loading){
-  return <p>Loading....</p>
-}
+  if (loading) {
+    return <p>Loading....</p>;
+  }
 
-if(error){
-  return <p>Error: {error.message}</p>
-}
-
-
-
-
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <>
-      <h1 className='text-black font-semibold'>Name : William Gyamfi Kumi .....YOLO Test</h1>
-      <CountryTable countries={countries}/>
+      <h1 className="text-black font-semibold">
+        Name : William Gyamfi Kumi .....YOLO Test
+      </h1>
+      <CountryTable countries={countries} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
